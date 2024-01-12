f7a5569dcc498a35f00d21d8c5b96a47c3469fd0
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2024-01-12 05:46:02 -0700

    Removed comma from html citation

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ project 1 - A Random Quote Generator
 // Quotes to randomly choose from
 const quotes = [{source: 'Albert Einstein', quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."},
                 {source: 'Bernard M. Baruch', quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind."},
-                {source: 'Mae West', quote: 'You only live once, but if you do it right, once is enough'},
-                {source: 'Martin Luther King Jr.', quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that'},
+                {source: 'Mae West', quote: 'You only live once, but if you do it right, once is enough.'},
+                {source: 'Martin Luther King Jr.', quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.'},
                 {source: 'Ralph Waldo Emerson', quote: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'},
                 {source: 'Judy Garland', quote: 'Toto, I\'ve a feeling we\'re not in Kansas anymore.', citation: 'The Wizard of Oz', year: '1939'},
                 {source: 'Hans Solo(Harrison Ford)', quote:'May the force be with you', citation: 'Star Wars', year: '1977'},
                 {source: 'Cuba Gooding Jr.', quote:'Show me the money!', citation: 'Jerry Maguire', year: '1996'}];
 
 
-
+// Creates a random quote
 function GetRandomQuote() {
       const randomNumber = Math.floor( Math.random() * quotes.length );
       const quote = quotes[randomNumber]
@@ -22,6 +22,7 @@ function GetRandomQuote() {
 }
 
 
+// Changes the background randomly by getting a random numberfor the r, g, and b in the rgb color
 function getRandomColor() {
     let r = Math.floor( Math.random() * 256 );
     let g = Math.floor( Math.random() * 256 );
@@ -29,6 +30,7 @@ function getRandomColor() {
     let rgb = [r, g, b];
     document.querySelector("body").attributeStyleMap.set("background-color", `rgb(${rgb.join(', ')})`); 
 }
+// Function calls background color change, creates the html, and adds citation and year if exist
 function printQuote() {
       getRandomColor();
       let quote = GetRandomQuote();
@@ -44,7 +46,7 @@ function printQuote() {
       document.getElementById('quote-box').innerHTML = html;
       
 }
-
+// Auto refreshes quote and background color 
 const autoQuote = setInterval(printQuote, 7000);
 /***
  * click event listener for the print quote button
